ff7d60dbcbaa8088872097f4c58bf8a39e3d4915
JingLin0 <jinglin.daisy@gmail.com>
2020-07-20 15:52:22 +0200

    delete schema related funcs and vars

--- tap_linkedin_ads/__init__.py
+++ tap_linkedin_ads/__init__.py
@@ -1,29 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
-import json
-import argparse
 import singer
-from singer import metadata, utils
+from singer import utils
 from tap_linkedin_ads.client import LinkedinClient
-from tap_linkedin_ads.discover import discover
 from tap_linkedin_ads.sync import sync
 
 LOGGER = singer.get_logger()
 
-REQUIRED_CONFIG_KEYS = [
-    'start_date',
-    'user_agent',
-    'access_token',
-    'accounts'
-]
-
-def do_discover():
-
-    LOGGER.info('Starting discover')
-    catalog = discover()
-    json.dump(catalog.to_dict(), sys.stdout, indent=2)
-    LOGGER.info('Finished discover')
+REQUIRED_CONFIG_KEYS = ["start_date", "user_agent", "access_token", "accounts"]
 
 
 @singer.utils.handle_top_exception(LOGGER)
@@ -31,19 +15,17 @@ def main():
 
     parsed_args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-    with LinkedinClient(access_token=parsed_args.config['access_token'],
-                        user_agent=parsed_args.config['user_agent']) as client:
+    with LinkedinClient(
+        access_token=parsed_args.config["access_token"],
+        user_agent=parsed_args.config["user_agent"],
+    ) as client:
         state = {}
         if parsed_args.state:
             state = parsed_args.state
+        sync(
+            client=client, config=parsed_args.config, state=state,
+        )
 
-        if parsed_args.discover:
-            do_discover()
-        elif parsed_args.catalog:
-            sync(client=client,
-                 config=parsed_args.config,
-                 catalog=parsed_args.catalog,
-                 state=state)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
